dbe61122b32abefb33b3daaa99b4bf0e487fb147
Andrew Faulkner <1897654@cegeplimoilou.ca>
2020-01-17 13:03:36 -0500

    New branch

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Navbar } from "react-bootstrap";
+
 
 class Navigation extends React.Component {
     render() {
-        <h1>Navigation</h1>
+        <Navbar>
+
+        </Navbar>
     }
 }
 
